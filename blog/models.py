8fd74343f0252082b982e25457745dea1af470f1
lyutian <lyutian623@163.com>
2017-07-25 22:48:07 +0800

    [initiate] define blog model and add a demo posting

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from ckeditor.fields import RichTextField
 
 # Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length = 100)
+    category = models.CharField(max_length = 50, blank = True)
+    date_time = models.DateTimeField(auto_now_add = True)
+    abstract = models.TextField(blank = True, null = True)
+    content = RichTextField()
+  
+    def __str__(self):
+        return self.title
+  
+    class Meta:
+        ordering = ['-date_time']
+
